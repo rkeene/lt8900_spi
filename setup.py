ae0c583259cf6c344b08a981ac80a69b18d5528d
rkeene <rkeene@noemail.net>
2020-04-28 21:14:41 +0000

    Converted to PyPI-compliant package
    FossilOrigin-Name: d304e9e5aeb9cd7012b185246e22ac8aca4c51195fe9d36dda0628aed24bb6e1

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="lt8900_spi-rkeene", # Replace with your own username
+	version="1.0",
+	author="Roy Keene",
+	author_email="pypi@rkeene.org",
+	description="Python interface to an LT8900 connected via SPI",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://chiselapp.com/user/rkeene/repository/lt8900_spi/",
+	packages=["lt8900_spi"],
+	package_dir={"":"."},
+	license="MIT License",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	install_requires=['spidev>=3.0.0']
+)
